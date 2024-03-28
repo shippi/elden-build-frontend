24cc076c14f43ed1d7e52111ae3fb5f9ded5d020
David <hn94@students.waikato.ac.nz>
2024-03-29 00:00:07 +1300

    login button disabled when either username or password is empty

--- components/Modals/LoginModal.tsx
+++ components/Modals/LoginModal.tsx
@@ -5,8 +5,13 @@ import { AuthContext } from "@/context/AuthContext"
 
 function LoginModal() {
     const {setLoginOpened} = useContext(AuthContext);
+    
     const [passwordVisible, setPasswordVisible] = useState(false);
-      const [loading, setLoading] = useState(false);
+    const [usernameInput, setUsernameInput] = useState<string>("");
+    const [passwordInput, setPasswordInput] = useState<string>("");
+
+    const [loading, setLoading] = useState(false);
+
     return (
         <div className="modal">
             <div className="modal-container">
@@ -17,16 +22,16 @@ function LoginModal() {
                         <div>
                             <label>Username</label>
                         </div>
-                        <input type="text" required/>
+                        <input type="text" value={usernameInput} onChange={e => setUsernameInput(e.target.value)} required/>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
                         <div className="password-container">
-                            <input type={passwordVisible ? "text" : "password"} required />
+                            <input type={passwordVisible ? "text" : "password"} value={passwordInput} onChange={e => setPasswordInput(e.target.value)} required />
                             <i className={"fa" + (passwordVisible ? " fa-eye-slash" : " fa-eye")} aria-hidden="true" onClick={() => setPasswordVisible(!passwordVisible)}/>
                         </div>
                     </div>
-                    <button type="submit" disabled={loading}>{loading ? <span className="spinner"></span> : "Log In"}</button>
+                    <button type="submit" className={!(passwordInput && usernameInput) ? "disabled" : ""} disabled={loading}>{loading ? <span className="spinner"></span> : "Log In"}</button>
                 </form>
             </div>
         </div>
