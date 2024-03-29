add28cb366e8d54fe395ef1144169461061f6b8a
David <d.nguyen7381@gmail.com>
2024-03-29 21:11:52 +1300

    implemented login functionality

--- components/Modals/LoginModal.tsx
+++ components/Modals/LoginModal.tsx
@@ -1,7 +1,10 @@
 'use client'
-import { useContext, useState } from "react"
+import { FormEvent, useContext, useState } from "react"
 import { ExitButton } from ".."
 import { AuthContext } from "@/context/AuthContext"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { delay } from "@/utils/SignUpUtils";
 
 function LoginModal() {
     const {setLoginOpened} = useContext(AuthContext);
@@ -9,18 +12,36 @@ function LoginModal() {
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [usernameInput, setUsernameInput] = useState<string>("");
     const [passwordInput, setPasswordInput] = useState<string>("");
+    const [error, setError] = useState<string>("");
 
     const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        setLoading(true);
+        delay(1000);
+        await signInWithEmailAndPassword(auth, usernameInput, passwordInput)
+        .then(() => setLoginOpened(false))
+        .catch(error => {
+            if (error.code == 'auth/wrong-password' || error.code == 'auth/invalid-credential') 
+                setError("Invalid log in details. Please try again.");
+            
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+    }
 
     return (
         <div className="modal">
             <div className="modal-container">
                 <ExitButton onClick={() => setLoginOpened(false)}/>
                 <div><h1>Log In</h1></div>
-                <form className="signup-form" autoComplete="off">
+                <form className="signup-form" autoComplete="off" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <div>
-                            <label>Username</label>
+                            <label>Email</label>
                         </div>
                         <input type="text" value={usernameInput} onChange={e => setUsernameInput(e.target.value)} required/>
                     </div>
@@ -31,6 +52,7 @@ function LoginModal() {
                             <i className={"fa" + (passwordVisible ? " fa-eye-slash" : " fa-eye")} aria-hidden="true" onClick={() => setPasswordVisible(!passwordVisible)}/>
                         </div>
                     </div>
+                    <span className="error">{error}</span>
                     <button type="submit" className={!(passwordInput && usernameInput) ? "disabled" : ""} disabled={loading}>{loading ? <span className="spinner"></span> : "Log In"}</button>
                 </form>
             </div>
--- services/authService.ts
+++ services/authService.ts
@@ -2,30 +2,23 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase";
 
 export async function signup(email: string, username: string, password: string) {
-    try {
-        let uid;
+    let uid;
 
-        const user = await createUserWithEmailAndPassword(auth, email, password)
-        .then(data => uid = data.user.uid);
+    const user = await createUserWithEmailAndPassword(auth, email, password)
+                        .then(data => uid = data.user.uid);
 
-        await fetch(process.env.NEXT_PUBLIC_API_URL + "users", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                id: uid,
-                email: email,
-                username: username
-            })
-        });
+    await fetch(process.env.NEXT_PUBLIC_API_URL + "users", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: uid,
+            email: email,
+            username: username
+        })
+    });
 
-        auth.signOut();
-
-        return user;
-    }
-    catch (error) {
-        return error;
-    }
+    auth.signOut();
 }
