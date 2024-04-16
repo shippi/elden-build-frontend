ee4299a038ae52921e3f41fa8289f72736c0ce9c
David <hn94@students.waikato.ac.nz>
2024-04-16 18:52:29 +1200

    builds/[id] fetches build

--- app/builds/[id]/page.tsx
+++ app/builds/[id]/page.tsx
@@ -2,8 +2,8 @@
 
 import { Loading, NotFound } from "@/components";
 import { AuthContext } from "@/context/AuthContext"
-import { useLoading } from "@/hooks";
-import { useContext } from "react"
+import { delay } from "@/utils";
+import { useContext, useEffect, useState } from "react"
 
 interface Props {
     params: {
@@ -13,6 +13,39 @@ interface Props {
 
 function Build({params: {id}}: Props) {
     const { currentUser } = useContext(AuthContext);
+    const [build, setBuild] = useState<any>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getBuild = async() => {
+            await fetch(process.env.NEXT_PUBLIC_API_URL + `builds/${id}`, {
+                method: "GET",
+                headers: {
+                  "Authorization" : `Bearer ${currentUser?.accessToken}` 
+                }
+            })
+            .then(res => {
+                if (!res.ok) throw Error;
+                return res.json()
+            }) 
+            .then(data => setBuild(data))
+            .catch(error => setBuild(null))
+            
+        }
+        getBuild();
+        setTimeout(() => setLoading(false), 750)
+        
+    }, [])
+    
+    return (
+        loading ? <Loading coverScreen={true}/>
+        :
+        !build ? <NotFound message="Build does not exist or it may be private."/>
+        :
+        <div>
+            {build.uid}
+        </div>
+    )
 }
 
 export default Build
