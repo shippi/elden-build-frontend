38aadd00729f69e6e083075b0c70ff83b01464d7
David <hn94@students.waikato.ac.nz>
2024-05-03 22:35:12 +1200

    contact details added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,33 @@
+import Link from "next/link"
+
 function Footer() {
     return (
+        <>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
         <div className="footer">
             <div className="footer-content">
-                <div>
+                <div className="contact">
                     <h3>Want to Report a Bug or Provide Feedback?</h3>
-                    <h4>Get in Touch</h4>
+                    <br/>
+                    <h4>Get in Touch:</h4>
+                    <div className="contacts-container">
+                        <div>
+                            <i className="bi bi-discord"/>
+                            .shippi
+                        </div>
+                        <Link href="https://github.com/shippi" target="_blank">
+                            <i className="bi bi-github"/>
+                            /shippi
+                        </Link>
+                        <Link href="https://www.reddit.com/user/shippiii/" target="_blank">
+                            <i className="bi bi-reddit"/>
+                            u/shippiii
+                        </Link>
+
+                    </div>
                 </div>
                 <div style={{borderLeft: "1px solid gray", height:"20px"}}/>
-                Change Logs and Todos
+                Changelogs and Todos
                 <div style={{borderLeft: "1px solid gray", height:"20px"}}/>
                 About
                 <div style={{borderLeft: "1px solid gray", height:"20px"}}/>
@@ -16,6 +36,7 @@ function Footer() {
                 Privacy
             </div>
         </div>
+        </>
     )
 }
 
