08a66d33d7485d856f2624c7617b2c1d58f7be75
David <hn94@students.waikato.ac.nz>
2024-05-08 18:27:26 +1200

    fixed typo

--- public/data/changelogs.ts
+++ public/data/changelogs.ts
@@ -4,7 +4,7 @@ export const changelogs = [
         changes: ["Initial release. Uses data from Elden Ring v1.10"]
     },
     {
-        date: "8rd May 2024",
+        date: "8th May 2024",
         changes: ["Fixed issue with not being able to switch Ashes of War on shields that start with 'No Skill'.",
             "Scorpion Charms and *-Shrouding Cracked Tears are now part of AR calculation."]
     }
